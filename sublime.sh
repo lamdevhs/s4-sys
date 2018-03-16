5de3622dac1c8e4ef26cbb23ad1c51fa35149d82
lamdevhs <lam.dev.hs@gmail.com>
2018-03-16 10:31:20 +0100

    sublime.sh

---
+++ sublime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+NAME=sublime
+FILE=~/$NAME.tar.bz2
+
+wget -O $FILE https://download.sublimetext.com/sublime_text_3_build_3143_x64.tar.bz2
+cd ~/
+bzip2 -d $FILE; tar xvf ~/$NAME.tar
+cd -
+~/sublime_text_3/sublime_text
+
+
