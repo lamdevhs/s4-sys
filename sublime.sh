54a4b28b789b4bfad62c8d2a14fb39cdf7da934a
lamdevhs <lam.dev.hs@gmail.com>
2018-04-06 12:01:39 +0200

    tp3-corbeille/

--- sublime.sh
+++ sublime.sh
@@ -8,6 +8,6 @@ wget -O $FILE https://download.sublimetext.com/sublime_text_3_build_3143_x64.tar
 cd ~/
 bzip2 -d $FILE; tar xvf ~/$NAME.tar
 cd -
-~/sublime_text_3/sublime_text
+~/sublime_text_3/sublime_text .
 
 
